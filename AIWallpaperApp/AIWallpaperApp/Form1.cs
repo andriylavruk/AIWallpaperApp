dc7bf003bb1d405ef11031f0a3bc2e64a51d6a6f
andriylavruk <andriy.lavruk.ua@gmail.com>
2024-11-04 20:29:48 +0200

    Update .gitignore. Fix generation image with empty prompt field. Add simple error handling to the btnGenerate_Click method.

--- AIWallpaperApp/AIWallpaperApp/Form1.cs
+++ AIWallpaperApp/AIWallpaperApp/Form1.cs
@@ -17,20 +17,33 @@ public partial class Form1 : Form
     private async void btnGenerate_Click(object sender, EventArgs e)
     {
         var prompt = textBoxPrompt.Text;
+
+        if (string.IsNullOrEmpty(prompt))
+        {
+            return;
+        }
+
         var response = await _imageService.GenerateImageAsync(prompt);
 
-        using (var ms = new MemoryStream(await response.Content.ReadAsByteArrayAsync()))
+        if (response.IsSuccessStatusCode)
         {
-            var image = Image.FromStream(ms);
+            using (var ms = new MemoryStream(await response.Content.ReadAsByteArrayAsync()))
+            {
+                var image = Image.FromStream(ms);
 
-            var imageName = @"img_" + Guid.NewGuid() + ".jpg";
-            var directory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName;
+                var imageName = @"img_" + Guid.NewGuid() + ".jpg";
+                var directory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName;
 
-            image.Save(imageName, ImageFormat.Jpeg);
-            FileInfo img__ = new FileInfo(imageName);
-            img__.MoveTo(directory + "\\Images\\" + imageName);
+                image.Save(imageName, ImageFormat.Jpeg);
+                FileInfo img__ = new FileInfo(imageName);
+                img__.MoveTo(directory + "\\Images\\" + imageName);
 
-            picBox.Image = image;
+                picBox.Image = image;
+            }
+        }
+        else
+        {
+            MessageBox.Show(response.StatusCode.ToString());
         }
     }
 }
